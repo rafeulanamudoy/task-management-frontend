5d16f5567bd934bd52697657f65b9a2852b4fd34
rafeulanamudoy <rafeuludoy@gmail.com>
2023-11-06 13:52:58 +0600

    client site host to netlify

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://task-management-backend-eta.vercel.app/api/v1",
   }),
   tagTypes: ["Task"],
 
